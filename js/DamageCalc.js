7c42a53143a666aa3c88b8305375aa5aab15d209
hiroscho <thomas__lehner@web.de>
2018-11-18 18:11:18 +0100

    -

--- js/DamageCalc.js
+++ js/DamageCalc.js
@@ -1,42 +1,42 @@
-var DamageCalc = {
-	data: {
-		skillDamage: 100000,
-		damageIncrease: 0,
-		enemyDefense: 250,
-		pierce: 0,
-		enemyResistance: 250,
-		specialPiercing: 0,
-		baseDamage: (skillDamage * (1 + (damageIncrease/100)))
-	},
-	
-	getDamageIncreaseFactor: function(double val) {
-		if(val < 0)
-			return 1;
-		else
-			return 1 + val;
-	},
+var DamageCalc = function() {
+	return {
+		data: {
+			skillDamage: 100000,
+			damageIncrease: 0,
+			enemyDefense: 250,
+			pierce: 0,
+			enemyResistance: 250,
+			specialPiercing: 0
+		},
+		
+		getDamageIncreaseFactor: function(double val) {
+			if(val < 0)
+				return 1;
+			else
+				return 1 + val;
+		},
 
-	getEnemyDefenseFactor: function() {
-		return 1/DamageCalc.data.enemyDefense;
-	},
+		getEnemyDefenseFactor: function() {
+			return 1/DamageCalc.data.enemyDefense;
+		},
 
-	getPierceFactor: function(double val) {
-		if(val < 0)
-			return 1;
-		else
-			return 1/(1-val);
-	},
+		getPierceFactor: function(double val) {
+			if(val < 0)
+				return 1;
+			else
+				return 1/(1-val);
+		},
 
-	getSpecialPiercingFactor: function(double val) {
-		if(val < 0)
-			return (1500 - DamageCalc.data.enemyResistance)/1500;
-		else
-			return (1500 - Math.max(DamageCalc.data.enemyResistance - (val * 15), 0))/1500;
-	},
+		getSpecialPiercingFactor: function(double val) {
+			if(val < 0)
+				return (1500 - DamageCalc.data.enemyResistance)/1500;
+			else
+				return (1500 - Math.max(DamageCalc.data.enemyResistance - (val * 15), 0))/1500;
+		},
 
-	getBaseDamage: function() {
-		return DamageCalc.data.skillDamage * DamageCalc.getDamageIncreaseFactor(0) * DamageCalc.getEnemyDefenseFactor() * DamageCalc.getPierceFactor(0) * DamageCalc.getSpecialPiercingFactor(0);
+		getBaseDamage: function() {
+			return DamageCalc.data.skillDamage * DamageCalc.getDamageIncreaseFactor(0) * DamageCalc.getEnemyDefenseFactor() * DamageCalc.getPierceFactor(0) * DamageCalc.getSpecialPiercingFactor(0);
+		}
 	}
-
 };
 	
